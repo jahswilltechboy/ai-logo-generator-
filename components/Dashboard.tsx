13498c0ef937a5bd6ccc5f29b66a31eda11862be
jahswilltechboy <jahswilltechboy@gmail.com>
2025-09-17 07:15:50 -0700

    Remove Generate sidebar item

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -83,15 +83,6 @@ const Dashboard: React.FC = () => {
         {/* Left Sidebar */}
         <aside className="w-64 bg-white border-r border-gray-200 min-h-screen p-4">
           <div className="space-y-6">
-            {/* Generate Section */}
-            <div>
-              <SidebarItem 
-                icon={<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>}
-                label="Generate" 
-                active={true}
-              />
-            </div>
-
             {/* Explore Section */}
             <div>
               <SidebarItem 
